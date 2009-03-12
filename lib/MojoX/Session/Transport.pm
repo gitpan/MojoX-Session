b80fca95091259a9783b038a10dbb8a673b38a29
Вячеслав Тихановский <vti@cpan.org>
2009-03-12 00:06:02 +0000

    Import of VTI/MojoX-Session-0.08 from CPAN.
    gitpan-cpan-distribution: MojoX-Session
    gitpan-cpan-version:      0.08
    gitpan-cpan-path:         VTI/MojoX-Session-0.08.tar.gz
    gitpan-cpan-author:       VTI
    gitpan-cpan-maturity:     released

--- lib/MojoX/Session/Transport.pm
+++ lib/MojoX/Session/Transport.pm
@@ -6,6 +6,10 @@ use warnings;
 use base 'Mojo::Base';
 
 require Carp;
+use Mojo::Transaction;
+
+__PACKAGE__->attr(
+    tx => (weak => 1, chained => 1, default => sub { Mojo::Transaction->new }));
 
 sub get { Carp::croak('Method "get" not implemented by subbclass') }
 
